5ad2dba6c2adaacb16cf0e9161e3f2846510c042
b2212100 <hiraku0209@icloud.com>
2023-11-29 19:20:35 +0900

    [ユーザ追加] ユーザ追加フォームを作成

--- wsbp/src/main/java/com/example/wsbp/page/UserMakerPage.java
+++ wsbp/src/main/java/com/example/wsbp/page/UserMakerPage.java
@@ -4,6 +4,12 @@ import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.wicketstuff.annotation.mount.MountPath;
 
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.PasswordTextField;
+import org.apache.wicket.markup.html.form.TextField;
+
+import org.apache.wicket.model.Model;
+
 @MountPath("UserMaker")
 public class UserMakerPage extends WebPage {
 
@@ -13,6 +19,35 @@ public class UserMakerPage extends WebPage {
          */
         var toHomeLink = new BookmarkablePageLink<>("toHome", HomePage.class);
         add(toHomeLink);
+
+        /**
+         * ユーザ追加フォームの作成
+         */
+        var userNameModel = Model.of("");
+        var userPassModel = Model.of("");
+
+        // 配置したFormコンポーネントを匿名クラス化して処理を上書きする
+        var userInfoForm = new Form<>("userInfo") {
+            @Override
+            protected void onSubmit() {
+                var userName = userNameModel.getObject();
+                var userPass = userPassModel.getObject();
+                var msg = "送信データ："
+                        + userName
+                        + ","
+                        + userPass;
+                System.out.println(msg);
+                // この1行を追加
+                setResponsePage(new UserMakerCompPage(userNameModel));
+            }
+        };
+        add(userInfoForm);
+
+        var userNameField = new TextField<>("userName", userNameModel);
+        userInfoForm.add(userNameField);
+
+        var userPassField = new PasswordTextField("userPass", userPassModel);
+        userInfoForm.add(userPassField);
     }
 
 }
